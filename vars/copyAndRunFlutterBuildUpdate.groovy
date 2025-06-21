96fd75f55b5c6fb31c72c4dbe4da957001c508da
MeherJOffice <meher@darasa.io>
2025-06-21 20:17:41 +0400

    Create copyAndRunFlutterBuildUpdate.groovy

---
+++ vars/copyAndRunFlutterBuildUpdate.groovy
@@ -0,0 +1,23 @@
+def call(String sdkFlutterProject, String rootPath) {
+    // Path to source and destination
+    def sourceFile = "${sdkFlutterProject}/flutterBuildUpdate"
+    def destFile = "${rootPath}/flutterBuildUpdate"
+
+    // Validate files/folders
+    if (!fileExists(sourceFile)) {
+        error "❌ Source executable not found: ${sourceFile}"
+    }
+
+    // Copy to rootPath
+    sh """
+        cp '${sourceFile}' '${destFile}'
+        chmod +x '${destFile}'
+    """
+    echo "✅ Copied flutterBuildUpdate to root: ${destFile}"
+
+    // Run it
+    sh """
+        '${destFile}'
+    """
+    echo "✅ Executed flutterBuildUpdate."
+}
